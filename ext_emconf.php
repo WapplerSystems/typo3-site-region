4fb9ca5b90a7444c34b70f0cafd850129c92f5f7
Sven Wappler <info@wappler.systems>
2022-02-02 16:01:48 +0100

    changes

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Site Region',
+    'description' => 'Additional region field fot sites',
+    'category' => 'plugin',
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3YYYY@wappler.systems',
+    'author_company' => 'WapplerSystems',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.0.0-10.5.99',
+            'fluid_styled_content' => '',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
